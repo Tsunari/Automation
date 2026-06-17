e745e079448bb75b0169385e50055df15cbe424d
Tsunari <tunahankakici@gmail.com>
2026-06-17 11:39:20 +0200

    feat(git-workflow-setup): allow 'other' commit type in commitlint rules

--- git-workflow-setup/src/templates.js
+++ git-workflow-setup/src/templates.js
@@ -6,7 +6,25 @@ export function getCommitlintTemplate() {
   "extends": ["@commitlint/config-conventional"],
   "rules": {
     "header-max-length": [0, "always"],
-    "body-max-line-length": [0, "always"]
+    "body-max-line-length": [0, "always"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "other"
+      ]
+    ]
   }
 }`
 }
